a41adc79769445958d07dee422ea2fc619d1ac99
Rails Udemy Course <webbc99@gmail.com>
2018-09-09 19:41:11 +0100

    Adjusted a lot of the CSS and changed the images to a carousel on the front page

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,12 @@
+function openForm() {
+    document.getElementById("app_form").style.display = "block";
+    document.getElementById("join_us").style.display = "none";
+}
+
+function closeForm() {
+    document.getElementById("app_form").style.display = "none";
+}
+
+function hideButton() {
+  document.getElementById("join_us").style.display = "none";
+}
